c4c759ef5782a0237874679aad0e4e38cac48f3b
Dagny Birgisdottir <Dagny Birgisdottir>
2015-11-29 12:38:00 +0000

    Adding header files and namespace std to Scientists_Repo Class

--- VLN-1/scientists_repo.cpp
+++ VLN-1/scientists_repo.cpp
@@ -1,7 +1,13 @@
 #include "scientists_repo.h"
+#include <fstream>
+#include <iostream>
+
+using namespace std;
 
 Scientists_Repo::Scientists_Repo()
 {
-
+    private:
+    int a = age;
+    vector
 }
 
