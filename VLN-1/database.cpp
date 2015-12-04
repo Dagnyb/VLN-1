9f368095df95f3455c5846f7efdcf2053db9a093
Halla Sigrún <hallas14@ru.is>
2015-12-04 19:22:11 +0000

    merge conflict

--- VLN-1/database.cpp
+++ VLN-1/database.cpp
@@ -1,7 +1,32 @@
 #include "database.h"
+#include <QtSql/QSql>
+#include "string"
+#include <iostream>
+#include <string>
+#include <stdexcept>
+
+
 
 Database::Database()
 {
+ /*   db.open();
+    QSqlQuery query(db);
+
+    query.prepare("INSERT INTO Scientists (Name, Gender, YearOfBirth, YearOfDeath) VALUES (Name, Gender, YearOfBirth, YearOfDeath)");
+    query.("Name",    QString::(userinterface.name()));
+    query.("Gender",  QString::(userinterface.gender()));
+    query.("YearOfBirth",     QString::(userinterface.birthYear()));
+    query.("YearOfDeath",     QString::(scientist.deathYear()));
+
+
+    query.exec();
+    QSqlDatabase db;
+    db = QSqlDatabase::addDatabase("QSQLITE");
+    QString dbScience = "dbScience.sqlite";*/
+
+
+
+
 
 }
 
